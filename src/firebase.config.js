057b49bfc0d0ab2c7aedd30f8b5c8db12454529f
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-05-01 18:42:42 +0200

    signin, signup, and routing with firebase setup

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNvq6_6PNdyZYhgxpFdPd6ZO8ufEFhI30",
+  authDomain: "media-com-feaa9.firebaseapp.com",
+  projectId: "media-com-feaa9",
+  storageBucket: "media-com-feaa9.appspot.com",
+  messagingSenderId: "489512348076",
+  appId: "1:489512348076:web:38f13586f76df279cb7209"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
