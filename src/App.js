917fd0457b6d36286dd1b41d881f220fc9934b52
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-05-02 11:48:34 +0200

    forgot password with firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
 import Explore from './pages/Explore'
+import PrivateRoute from './components/PrivateRoutes'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
@@ -13,7 +14,9 @@ function App() {
        <Router>
         <Routes>
           <Route path='/' element={<Explore />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+             <Route path='/profile' element={<Profile />} />
+          </Route> 
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} /> 
